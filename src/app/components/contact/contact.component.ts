e99d9a24b2cc7cd13cba78679406bfa9bc934c09
Cameron Gilbert <gilbertjcameron@gmail.com>
2021-08-24 17:14:01 -0400

    s

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -7,6 +7,9 @@ import { Subject } from 'rxjs';
   templateUrl: './contact.component.html',
 })
 export class ContactComponent implements OnDestroy, OnInit {
+  public name = '';
+  public subject = '';
+  public body = '';
   public componentDestroyed: Subject<boolean> = new Subject();
 
   constructor(private metaTagService: Meta, private titleService: Title) {}
@@ -22,5 +25,21 @@ export class ContactComponent implements OnDestroy, OnInit {
   public ngOnDestroy(): void {
     this.componentDestroyed.next(true);
     this.componentDestroyed.complete();
+  }
+
+  public onChangeName(e: Event): void {
+    this.name = encodeURIComponent((e.target as HTMLInputElement).value);
+  }
+
+  public onChangeSubject(e: Event): void {
+    this.subject = encodeURIComponent((e.target as HTMLInputElement).value);
+  }
+
+  public onChangeBody(e: Event): void {
+    this.body = encodeURIComponent((e.target as HTMLTextAreaElement).value);
+  }
+
+  public getAttr(): string {
+    return `mailto:wkgilbert95@gmail.com?subject=${this.subject}&body=${this.name}%0A${this.body}`;
   }
 }
